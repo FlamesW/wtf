f2ea1d7c0d2208cd70b7761e0ba2d3ccd1eb47b4
FlamesW <undergodbreaker@gmail.com>
2026-02-06 20:29:44 +0200

    Create ChangelogsTab.lua

---
+++ Resources/ChangelogsTab.lua
@@ -0,0 +1,49 @@
+return {
+    SupportedGames = {
+        {Game = "Grand Piece Online", Status = "Updating", Date = "03/12/2025"},
+        {Game = "A Universal Time", Status = "Online", Date = "29/11/2025"},
+        {Game = "Your Bizzare Adventure", Status = "Online", Date = "06/12/2025"},
+    },
+
+    Changelogs = {
+        {
+            ["Title"] = "Your Bizzare Adventure 1.04",
+            ["Date"] = "06/12/2025",
+            ["Changes"] = {
+                "- Added Your Bizzare Adventure",
+                "- In BETA, Report any bugs",
+            },
+            ["Note"] = "Note: Updates may take some time.",
+        },
+
+        {
+            ["Title"] = "Grand Piece Online 1.03",
+            ["Date"] = "03/12/2025",
+            ["Changes"] = {
+                "- Fixed Auto Fishing AFK Ban",
+                "- Level Farm now more Stable",
+                "- Added Tween Speed Modifier",
+            },
+            ["Note"] = "Note: Updates may take some time.",
+        },
+
+        {
+            ["Title"] = "Grand Piece Online 1.02",
+            ["Date"] = "29/11/2025",
+            ["Changes"] = {
+                "- Added Grand Piece Online",
+                "- In BETA, Report any bugs",
+            },
+            ["Note"] = "Note: Updates may take some time.",
+        },
+
+        {
+            ["Title"] = "Welcome to Flameh 1.01",
+            ["Date"] = "N/A",
+            ["Changes"] = {
+                "- This is the beginning of our journey:)~",
+            },
+            ["Note"] = "Note: Nothing",
+        },
+    }
+}
