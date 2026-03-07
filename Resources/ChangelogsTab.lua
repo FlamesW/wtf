e08ce55e486075cdf12bce48fa3d9c6c8b64b602
Flameh <undergodbreaker@gmail.com>
2026-03-07 12:05:53 +0200

    Add new game 'Escape For Brainrots' to supported games

--- Resources/ChangelogsTab.lua
+++ Resources/ChangelogsTab.lua
@@ -1,6 +1,7 @@
 return {
     SupportedGames = {
         {Game = "World Of Stands", Status = "Online", Date = "28/02/2026"},
+        {Game = "Escape For Brainrots", Status = "Offline", Date = "07/03/2026"},
     },
 
     Changelogs = {
