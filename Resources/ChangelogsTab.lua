2054fabe90bb3223749038633762b6d7d3391145
Flameh <undergodbreaker@gmail.com>
2026-03-20 19:45:59 +0200

    Update changelogs and add Swordflare RPG
    Added a new game 'Swordflare RPG' and updated changelogs.

--- Resources/ChangelogsTab.lua
+++ Resources/ChangelogsTab.lua
@@ -1,10 +1,26 @@
 return {
     SupportedGames = {
         {Game = "World Of Stands", Status = "Online", Date = "28/02/2026"},
-        {Game = "Escape For Brainrots", Status = "Offline", Date = "07/03/2026"},
+        {Game = "Swordflare RPG", Status = "Online", Date = "20/03/2026"},
     },
 
     Changelogs = {
+        {
+            ["Title"] = "Flameh 1.03",
+            ["Date"] = "20/03/2026",
+            ["Changes"] = {
+                "- Added Swordflare RPG",
+            },
+            ["Note"] = "Note: Nothing",
+        },
+        {
+            ["Title"] = "Flameh 1.02",
+            ["Date"] = "28/02/2026",
+            ["Changes"] = {
+                "- Added World Of Stands",
+            },
+            ["Note"] = "Note: Nothing",
+        },
         {
             ["Title"] = "Welcome to Flameh 1.01",
             ["Date"] = "N/A",
